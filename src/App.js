2ff1a5ac9c3db9bcd41dcc168ad1d59d2d893738
priyansh <priyanshv03@gmail.com>
2022-04-23 01:50:44 +0530

    working hard :0)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Navbar from './component/Navbar';
-
+import Carousel2 from './component/Carousel2';
+import Categories from './component/Categories';
+import Footer from './component/Footer';
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+     
+      <Categories />
+      <Carousel2 />
+      <Footer/>
     </>
   );
 }
---
+++ src/component/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import "./Footer.css"
+
+// let Card =(props)=>{
+//     return (<div className="card">
+//         <div className='img-left'>
+//      <img src="https://blinkit.com/images/home/express-delivery-icon-93fce76.png" alt="" />
+//         </div>
+//      <div className="card_info">
+//         <div className="details">
+//             <span className="card_title_footer">{props.Title}</span>
+//             <span className='Adjective '>{props.Adjective}</span>
+//          </div>
+//      </div>
+//  </div>
+//  )
+//  }
+
+export default function 
+() {
+    return (<>
+   <div id="footer-width">
+            <hr />
+            <div className="size">We deliver everything in 10 minutes</div> 
+
+blinkit is India's fastest online shopping platform. Get fresh groceries and much more delivered online instantly.
+
+<div className="size">Single app for all your daily needs</div>
+Order thousands of products at just a tap: milk, eggs, bread, cooking oil, ghee, atta, rice, fresh fruits and vegetables, spices, fresh meat, seafood, frozen veg and non-veg food, chocolates, chips, biscuits, maggi, cold drinks, shampoos, soaps, body wash, pet food, diapers, electronics, sanitizers, masks, sanitary napkins, health products, hygiene products, organic products and gourmet products, and much more from your neighbourhood stores.
+
+ <div className="size">Available in following citiesh</div>
+            The delivery service is operational in Ahmedabad, Bengaluru, Chandigarh, Chennai, Delhi, Faridabad, Gurgaon, Haridwar, HR-NCR, Hyderabad, Jaipur, Jalandhar, Kanpur, Kolkata, Lucknow, Ludhiana, Meerut, Mohali, Mumbai, Panchkula, Pune, UP-NCR, Vadodara, Zirakpur
+            
+            <div className='main-border'>
+                <div className="item1">
+                    <div className='imgs'>
+                        <img className='imgs' src="https://blinkit.com/images/home/express-delivery-icon-93fce76.png" alt="" />
+                        </div> 
+                    <div className='texts'>
+                       <h6>10 minute grocery now</h6><br />
+                       Get your order delivered to your doorstep at the earliest from dark stores near you.                     
+                        
+                  </div>
+                </div>
+
+            </div>
+        </div>
+      
+
+  </>
+   
+  )
+}
